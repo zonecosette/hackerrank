a8b913fe93ce9c17393f6efc8d3f4ba3efe90ebf
zonecosette <38497933+zonecosette@users.noreply.github.com>
2021-04-08 14:49:31 +0700

    Add files via upload

---
+++ Algorithms/7_Staircase.cs
@@ -0,0 +1,37 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        for (int y = n - 1; y >= 0; y--)
+        {
+            for (int x = 0; x < n; x++)
+            {
+                if (x >= y)
+                    Console.Write("#");
+                else
+                    Console.Write(" ");
+            }
+        Console.WriteLine("");;
+        }
+    }
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
