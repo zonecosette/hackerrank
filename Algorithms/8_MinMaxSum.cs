74b73af785c79854e94975dd213f44b24ad7d87f
zonecosette <38497933+zonecosette@users.noreply.github.com>
2021-04-09 17:35:28 +0700

    Add files via upload

---
+++ Algorithms/8_MinMaxSum.cs
@@ -0,0 +1,31 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        double sum1 = arr.Sum(a => (double)a) - arr.Max(a => (double)a);
+        double sum2 = arr.Sum(a => (double)a) - arr.Min(a => (double)a);
+        
+        Console.WriteLine($"{sum1} {sum2}");
+
+    }
+
+    static void Main(string[] args) {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        miniMaxSum(arr);
+    }
+}
